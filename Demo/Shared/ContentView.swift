27faa26772e95b71229f3c0e9b00b7423135da31
Taylor Holliday <taylor@audulus.com>
2022-05-28 18:38:28 -0700

    Add demo project.

---
+++ Demo/Shared/ContentView.swift
@@ -0,0 +1,16 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKitUI/
+
+import SwiftUI
+import PianoRoll
+
+struct ContentView: View {
+    var body: some View {
+        PianoRollTestView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/Shared/PianoRollDemoApp.swift
@@ -0,0 +1,12 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKitUI/
+
+import SwiftUI
+
+@main
+struct PianoRollDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

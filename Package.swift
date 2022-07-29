35175956e24b70af4553dbf33cf10100701cc8bb
Aurelius Prochazka <aurephd@gmail.com>
2022-07-28 17:56:52 -0700

    Tighten up Package.swift

--- Package.swift
+++ Package.swift
@@ -5,18 +5,6 @@ import PackageDescription
 let package = Package(
     name: "PianoRoll",
     platforms: [ .macOS(.v12), .iOS(.v15)],
-    products: [
-        .library(
-            name: "PianoRoll",
-            targets: ["PianoRoll"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "PianoRoll",
-            dependencies: []),
-        .testTarget(
-            name: "PianoRollTests",
-            dependencies: ["PianoRoll"]),
-    ]
+    products: [.library(name: "PianoRoll", targets: ["PianoRoll"])],
+    targets: [.target(name: "PianoRoll", dependencies: [])]
 )

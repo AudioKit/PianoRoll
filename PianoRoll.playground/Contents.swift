aadd62029fd54ea88cb0bbbb3661ec52f9d5fb10
Aurelius Prochazka <aurephd@gmail.com>
2022-07-28 21:57:31 -0700

    Added playground

---
+++ PianoRoll.playground/Contents.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import PlaygroundSupport
+import PianoRoll
+
+struct PianoRollDemoView: View {
+
+    @State var model = PianoRollModel(notes: [
+        PianoRollNote(start: 1, length: 2, pitch: 9),
+        PianoRollNote(start: 5, length: 1, pitch: 4)
+    ], length: 12, height: 12)
+
+    public var body: some View {
+        ScrollView([.horizontal, .vertical], showsIndicators: true) {
+            PianoRoll(model: $model, noteColor: .cyan)
+        }.background(Color(white: 0.1))
+    }
+}
+
+PlaygroundPage.current.setLiveView(PianoRollDemoView().frame(width: 500, height: 500))
+PlaygroundPage.current.needsIndefiniteExecution = true
